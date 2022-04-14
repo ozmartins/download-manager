5100ce83dbf9b46328fcb1af3d8d6bca861bc99f
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2022-04-13 22:26:57 -0300

    Add empty history form

---
+++ DownloadManager.Vcl/Source/Desktop/History.pas
@@ -0,0 +1,24 @@
+unit History;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  THistoryForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  HistoryForm: THistoryForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
