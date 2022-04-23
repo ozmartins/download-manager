ee705dcfca3424bb31158fb9f62b6bff6294b6a0
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2022-04-22 22:56:04 -0300

    Finish HttpHeaderHelper.pas XML documentation.

--- DownloadManager.Vcl/Source/Infra/Network/HttpHeaderHelper.pas
+++ DownloadManager.Vcl/Source/Infra/Network/HttpHeaderHelper.pas
@@ -6,11 +6,6 @@ uses
   DomainConsts, System.Net.HttpClient;
 
 type
-  /// <summary>
-  /// This is a class used to extract data from Content-Disposition.
-  /// Content-Disposition is an HTTP header field, which can indicate
-  /// if the HTTP response content should be downloaded or displayed.
-  /// </summary>
   THttpHeaderHelper = class
   public
     class function ExtractFileNameFromHeader(AHttpResponse: IHttpResponse): String;
@@ -22,9 +17,11 @@ uses System.SysUtils, InfraConsts;
 
 { TContentDispositionHelper }
 
-/// <summary>Receives a parameter in the following format: 'attachment; filename="file.txt'". So, the function extracts the file name portion from this parameter</summary>
-/// <param name="AContentDisposition">Content-Disposition is one of the fields in the HTTP header. So get the Content-Disposition from an HTTP header and call this function to extract the file name (if it exists).</param>
-/// <returns>The file name present in the content-disposition parameter, or an empty string if the file name can't be extracted</returns>
+/// <summary>Receives an IHttpResponse parameter and looks for the content-disposition field.
+/// Content-disposition has the following format: 'attachment; filename="file.txt'".
+/// So, the function tries to extract the file name portion from this field.</summary>
+/// <param name="AHttpResponse">The response from a HTTP request.</param>
+/// <returns>The file name is present in the content-disposition field in the response or an empty string if the file name can't be extracted.</returns>
 class function THttpHeaderHelper.ExtractFileNameFromHeader(AHttpResponse: IHttpResponse): String;
 var
   lContentDisposition: String;
